2934c515a67c7abb89deab837310a762e9f1e5e5
Zeynep <melisaari81@gmail.com>
2026-02-18 15:06:55 +0200

    Added pop-up windows for add->add, edit->edit error handling (before it was just add->edit, edit->add).

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,28 +21,28 @@
     <!-- RIGHT PANEL -->
     <div class="right" ref="rightPanel" :style="{ width: rightWidth + '%' }">
       <!-- Show this form when adding a new note -->
-      <div v-if="adding && !showPopupEdit" class="adding-wrapper">
+      <div v-if="adding && showPopup != 2 || showPopup == 0" class="adding-wrapper">
         <form class="note-form" @submit.prevent="createNote">
           <div class="notes-content">
             <input class="title-input" v-model="newTitle" placeholder="Title" />
             <textarea class="content-input" v-model="newContent" placeholder="Content"></textarea>
           </div>
           <div class="form-actions">
-            <button type="submit" class="primary">Add note</button>
+            <button type="button" @click="addNote" class="primary">Add note</button>
             <button type="button" @click="cancelAdding">Cancel</button>
           </div>
         </form>
       </div>
 
       <!-- Show this form when editing an existing note -->
-      <div v-if="editing && !showPopupAdd" class="editing-wrapper">
+      <div v-if="editing && showPopup != 3 || showPopup == 1" class="editing-wrapper">
           <form class="edit-note-form" @submit.prevent="startEdit">
           <div class="edit-notes-content">
             <input class="edit-title-input" v-model="editTitle"/>
             <textarea class="edit-content-input" v-model="editContent"></textarea>
           </div>
           <div class="edit-form-actions">
-            <button type="submit" class="primary" @click="updateNote">Save</button>
+            <button type="button" class="primary" @click="updateNote">Save</button>
             <button type="button" @click="cancelEdit">Cancel</button>
           </div>
         </form>
@@ -50,24 +50,46 @@
       </div>
     </div>
 
-    <!-- Popup for when there are unsaved changes while switching from editing to adding -->
-    <div v-if="showPopupEdit" class="overlay">
+    <!-- Popup for when there are unsaved changes while switching from creating a note to adding another -->
+    <div v-if="showPopup == 0" class="overlay">
       <div class="popup">
         <p>You have unsaved changes. Do you want to discard them?</p>
         <div class="popup-actions">
-          <button class="primary" @click="showPopupEdit = false; cancelEdit(); createNote();">Discard</button>
-          <button type="resume" @click="showPopupEdit = false; adding = false;">Continue</button>
+          <button class="primary" @click="showPopup = -1; cancelAdding(); createNote();">Discard</button>
+          <button type="resume" @click="showPopup = -1;">Continue</button>
+        </div>
+        </div>
+    </div>
+
+    <!-- Popup for when there are unsaved changes while switching from editing a note to editing another -->
+    <div v-if="showPopup == 1" class="overlay">
+      <div class="popup">
+        <p>You have unsaved changes. Do you want to discard them?</p>
+        <div class="popup-actions">
+          <button class="primary" @click="showPopup = -1; cancelEdit(); startEdit(currentNote);">Discard</button>
+          <button type="resume" @click="showPopup = -1;">Continue</button>
+        </div>
+        </div>
+    </div>
+
+    <!-- Popup for when there are unsaved changes while switching from editing to adding -->
+    <div v-if="showPopup == 2" class="overlay">
+      <div class="popup">
+        <p>You have unsaved changes. Do you want to discard them?</p>
+        <div class="popup-actions">
+          <button class="primary" @click="showPopup = -1; cancelEdit(); createNote();">Discard</button>
+          <button type="resume" @click="showPopup = -1; adding = false;">Continue</button>
         </div>
         </div>
     </div>
 
       <!-- Popup for when there are unsaved changes while switching from adding to editing -->
-      <div v-if="showPopupAdd" class="overlay">
+      <div v-if="showPopup == 3" class="overlay">
         <div class="popup">
           <p>You have unsaved changes. Do you want to discard them?</p>
           <div class="popup-actions">
-            <button class="primary" @click="showPopupAdd = false; cancelAdding(); startEdit(currentNote);">Discard</button>
-            <button type="resume" @click="showPopupAdd = false; editing = false;">Continue</button>
+            <button class="primary" @click="showPopup = -1; cancelAdding(); startEdit(currentNote);">Discard</button>
+            <button type="resume" @click="showPopup = -1; editing = false;">Continue</button>
           </div>
         </div>
     </div>
@@ -91,50 +113,58 @@ export default {
       editTitle: '',
       editContent: '',
       isResizing: false,
-      showPopupEdit: false,
-      showPopupAdd: false,
+      showPopup: -1,/* 0->adding to adding, 1->editing to editing, 2->editing to adding, 3->adding to editing */
       currentNote: null,
       leftWidth: 50,
       rightWidth: 50
     }
   },
 
-  // When the component loads, it automatically calls fetchNotes() to load all saved notes from backend
+  /* When the component loads, it automatically calls fetchNotes() to load all saved notes from backend */
   created() {
     this.fetchNotes()
   },
 
   methods: {
-    // async means the function can use await inside it to pause until Promises resolve
-    // fetch(...) starts an HTTP request to the URL ${API}/notes
+    /* async means the function can use await inside it to pause until Promises resolve
+     * fetch(...) starts an HTTP request to the URL ${API}/notes */
     async fetchNotes() {
       const res = await fetch(`${API}/notes`)
       this.notes = await res.json()
     },
 
-    async createNote() {
+    createNote() {
+      if(this.adding) {
+        if(this.newTitle !== '' || this.newContent !== '') {
+          this.showPopup = 0
+        }
+        return
+      }
+
       this.adding = true;
 
-      // If uou're currently editing a note and click "New", check for unsaved changes before switching to the add form
+      /* If you're currently editing a note and click "New", check for unsaved changes before switching to the add form */
       if(this.editing) {
           if(this.currentNote.title !== this.editTitle || this.currentNote.content !== this.editContent) {
-            this.showPopupEdit = true
+            this.showPopup = 2
           }
           else {
             this.cancelEdit()
           }
         return
       }
+    },
 
+    async addNote() {
       if (!this.newTitle) return
       const res = await fetch(`${API}/notes`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ title: this.newTitle, content: this.newContent }) // Converts a JavaScript object into a JSON-formatted string
-      })
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({ title: this.newTitle, content: this.newContent }) // Converts a JavaScript object into a JSON-formatted string
+        })
       
       
-      // res.ok is a shortcut property on the Response object that is true when the HTTP status code is in the 200–299 range
+      /* res.ok is a shortcut property on the Response object that is true when the HTTP status code is in the 200–299 range */
       if (res.ok) {
         this.cancelAdding()
         await this.fetchNotes()
@@ -143,25 +173,39 @@ export default {
       }
     },
 
+    cancelAdding() {
+      this.adding = false
+      this.newTitle = ''
+      this.newContent = ''
+    },
+
     async deleteNote(id) {
       const res = await fetch(`${API}/notes/${id}`, { method: 'DELETE' })
       
-      // 204 is the special "No Content" status — commonly used to indicate a successful deletion where the server returns no body
+      /* 204 is the special "No Content" status — commonly used to indicate a successful deletion where the server returns no body */
       if (res.status === 204) this.fetchNotes()
       this.editing = false
     },
 
     startEdit(n) {
-      this.editing = true
       this.currentNote = n
+
+      if(this.editing) {
+        if(this.currentNote.title !== this.editTitle || this.currentNote.content !== this.editContent) {
+          this.showPopup = 1
+          return
+        }
+      }
+
+      this.editing = true
       this.editId = n.id
       this.editTitle = n.title
       this.editContent = n.content
 
-      // If you're currently adding a new note and click on an existing note to edit, check for unsaved changes before switching to the edit form
+      /* If you're currently adding a new note and click on an existing note to edit, check for unsaved changes before switching to the edit form */
       if(this.adding) {
         if(this.newTitle !== '' || this.newContent !== '') {
-          this.showPopupAdd = true
+          this.showPopup = 3
         }
         else {
           this.cancelAdding()
@@ -174,13 +218,6 @@ export default {
       this.editing = false
       this.editId = null
     },
-
-    cancelAdding() {
-      this.adding = false
-      this.newTitle = ''
-      this.newContent = ''
-    },
-
 
     async updateNote() {
       const res = await fetch(`${API}/notes/${this.editId}`, {
